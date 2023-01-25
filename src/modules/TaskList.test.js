9bd1eb2f16711fad55348d29974f4d4912bc642b
dfsalam <dfsalam@gmail.com>
2023-01-25 17:15:16 -0500

    Add testing for the remove function

--- src/modules/TaskList.test.js
+++ src/modules/TaskList.test.js
@@ -13,22 +13,38 @@ describe('tasks', () => {
   const container = document.querySelector('#to-do-list');
   const task_list = new TaskList(container);
   task_list.add('Do the bed.');
+  task_list.add('Do the bed.');
   //Add testing
-  test('add function', () => {
+  test('check the array for adding elements', () => {
     
     expect(task_list.taskArray).toHaveLength(1);
   })
-  test('local storage', () => {
+  test('check local storage for add', () => {
     
-    const localStorageRegister = JSON.parse(localStorage.getItem('taskList'))
-    console.log(localStorageRegister);
+    const localStorageRegister = JSON.parse(localStorage.getItem('taskList'))    
     delete localStorageRegister[0].tag;
     expect(localStorageRegister).toStrictEqual([{description:'Do the bed.',
      index:1, completed:false}]);
   })
+  
   //Delete testing
-  task_re
+  
+  const btn = document.querySelector('.remove__button');
+  console.log(btn);
+  task_list.remove(btn);
 
+  test('check the array for removing elements', () => {    
+    expect(task_list.taskArray).toHaveLength(1);
+  })
 
-
+  test('check local storage for removing', () => {    
+    const localStorageRegister = JSON.parse(localStorage.getItem('taskList'));
+       
+    delete localStorageRegister[0].tag
+    expect(localStorageRegister).toStrictEqual([{description:'Do the bed.',
+    index:1, completed:false}]);
+  })
 })
+
+
+  
